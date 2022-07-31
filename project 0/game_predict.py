60c9d084350e604b37cbc07c2a66b0091784b3f1
Evgeny Chitaev <eachitaev@gmail.com>
2022-07-31 22:22:25 +0300

    Predict

--- project 0/game_predict.py
+++ project 0/game_predict.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Программа угадывает числа
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -35,7 +35,7 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = [] # список для сохранения количества попыток
-    # np.random.seed(1) # фиксируем сид для воспроизводимости
+    np.random.seed(1) # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
     for number in random_array:
         count_ls.append(random_predict(number))
